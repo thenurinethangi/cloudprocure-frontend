9ca0e95f6c9e6692976fd981f1a5e9752c0067fb
thenurinethangi <thenurinathangi@gmail.com>
2026-08-23 17:08:26 +0530

    Configure Cloud Run API proxy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://34.102.213.236/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
